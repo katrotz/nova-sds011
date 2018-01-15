b567116be0f5d9f9e878a072b01af5a05a4a8302
katrotz <katrotz@gmail.com>
2018-01-15 22:44:23 +0100

    Makes the code espruino interpreter compatible

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 'use strict'
 // 
-// Script to connect to the Nova SDS011, read PM values from the serial port 
-// and write them to the console.
+// Script to connect to the Nova SDS011, read PM values from the serial port and write them to the console.
+// Usage: `node index.js /dev/ttyUSB0`
 //
-let serialport = require("serialport")
-let sds011 = require('./lib/sds011')
+const SerialPort = require('serialport');
+const sds011 = require('./lib/sds011')
+const port = process.argv[2] || '/dev/ttyUSB0';
 
-let serial = new serialport.SerialPort("/dev/ttyUSB0", {
+const serial = new SerialPort(port, {
     baudrate: 9600
-})
+});
+
 serial.on('open', function () {
-    console.log('Connected')
+    console.log('Connected');
 })
 
 serial.on('error', function (err) {
-    console.error('Failed to connect:', err)
-    process.exit(-1)
+    console.error('Failed to connect:', err);
+    process.exit(-1)''
 })
 
 serial.on('data', function (data) {
-    let pmValues = sds011(data)
+    const pmValues = sds011(data);
+
     if (!pmValues) {
-        log.error('failed to parse buffer [' + data.toString('hex') + ']')
-        return
+        log.error('Failed to parse buffer [' + data.toString('hex') + ']');
+        return;
     }
-    console.info("pm2.5: " + pmValues.pm2_5 + "\tpm10: " + pmValues.pm10)
+
+    console.info("pm2.5: " + pmValues.pm2_5 + "\tpm10: " + pmValues.pm10);
 });
